77f9d66d58bf47d6d3369d8a149ba89e183caa91
ogzCode <oguzguc44@gmail.com>
2025-06-26 14:23:14 +0300

    Enhance Django admin user management by customizing UserAdmin to display user groups

--- core/auth_api/admin.py
+++ core/auth_api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+
+def get_user_groups(obj):
+    if hasattr(obj, 'user'):
+        return ', '.join([group.name for group in obj.user.groups.all()])
+    else:
+        return ', '.join([group.name for group in obj.groups.all()])
+
+
+get_user_groups.short_description = 'Gruplar'
+
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name',
+                    'is_staff', get_user_groups)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
